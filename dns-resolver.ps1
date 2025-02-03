869d6bc38143a795aae538ef6db93178dbe607ac
benjamintyler367 <benjamin.tyler@mymail.champlain.edu>
2025-02-03 13:00:59 -0500

    Add PowerShell script

---
+++ dns-resolver.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$NetworkPrefix,
+
+    [Parameter(Mandatory=$true)]
+    [string]$DnsServer
+)
+
+$startIp = 1
+$endIp = 254
+
+for ($i = $startIp; $i -le $endIp; $i++) {
+    $ipAddress = "$NetworkPrefix.$i"
+    try {
+        $result = Resolve-DnsName -Name $ipAddress -Server $DnsServer -Type PTR -ErrorAction Stop
+        Write-Output "IP: $ipAddress, Hostname: $($result.NameHost)"
+    }
+    catch {
+        Write-Output "IP: $ipAddress, No reverse DNS record found"
+    }
+
+}
+
